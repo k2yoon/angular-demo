03b2efc411aedd86d21f02bcedfa47dfba421148
Kai Yoon <kai.yoon@indexexchange.com>
2018-10-11 11:49:36 -0400

    changing breakpoints to xsmall to xlarge

--- src/app/kai-component1/kai-component1.component.ts
+++ src/app/kai-component1/kai-component1.component.ts
@@ -9,18 +9,40 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./kai-component1.component.css']
 })
 export class KaiComponent1Component implements OnInit {
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+  // isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.HandsetPortrait)
+  //   .pipe(
+  //     map(result => result.matches)
+  //   );
+  // isTablet$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.TabletPortrait)
+  //   .pipe(
+  //     map(result => result.matches)
+  //   );
+  // isWeb$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.WebPortrait)
+  //   .pipe(
+  //     map(result => result.matches)
+  //   );
+
+  isXsmall$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XSmall)
     .pipe(
       map(result => result.matches)
     );
-  isTablet$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Tablet)
+  isSmall$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Small)
     .pipe(
       map(result => result.matches)
     );
-  isWeb$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Web)
+  isMedium$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Medium)
     .pipe(
       map(result => result.matches)
     );
+  isLarge$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Large)
+    .pipe(
+      map(result => result.matches)
+    );
+  isXlarge$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.XLarge)
+    .pipe(
+      map(result => result.matches)
+    );
+
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
